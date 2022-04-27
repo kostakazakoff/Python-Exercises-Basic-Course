a11c0bd446565a1ed84a43e8ec5935d5e5bc59f7
Kosta Kazakov <kostakazakoff@gmail.com>
2022-04-27 03:19:52 +0300

    Update longer_line.py

--- longer_line.py
+++ longer_line.py
@@ -10,32 +10,40 @@ y4 = float(input())
 
 def longer_line(l1_x1, l1_y1, l1_x2, l1_y2, l2_x1, l2_y1, l2_x2, l2_y2):
     import math
-
+    
+    # define a point coordinates:
     p1 = [l1_x1, l1_y1]
     p2 = [l1_x2, l1_y2]
     p3 = [l2_x1, l2_y1]
     p4 = [l2_x2, l2_y2]
-
+    
+    # First line:
+    # calculate a points distance from the center of the Cartesian coordinate system:
     point1_distance = abs(p1[0]) ** 2 + abs(p1[1]) ** 2
     point2_distance = abs(p2[0]) ** 2 + abs(p2[1]) ** 2
+    
+    # define a line direction (nearest to 0,0 point is the start point):
     if point2_distance < point1_distance:
         line_1 = [p2, p1]
     else:
         line_1 = [p1, p2]
-
+    
+    # Second line:
     point3_distance = abs(p3[0]) ** 2 + abs(p3[1]) ** 2
     point4_distance = abs(p4[0]) ** 2 + abs(p4[1]) ** 2
     if point4_distance < point3_distance:
         line_2 = [p4, p3]
     else:
         line_2 = [p3, p4]
-
+    
+    # Compare the lines length (the result is the longer line):
     l1_length = (line_1[1][0] - line_1[0][0]) ** 2 + (line_1[1][1] - line_1[0][1]) ** 2
     l2_length = (line_2[1][0] - line_2[0][0]) ** 2 + (line_2[1][1] - line_2[0][1]) ** 2
     if l2_length > l1_length:
         result = line_2
     else:
         result = line_1
+    
     p1_output = tuple(math.floor(x) for x in result[0])
     p2_output = tuple(math.floor(x) for x in result[1])
     output = str(p1_output) + '' + str(p2_output)
