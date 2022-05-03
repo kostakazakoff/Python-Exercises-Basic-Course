8ca386fe4cb95c8788050df6eb4fff7f68ba5171
Kosta Kazakov <kostakazakoff@gmail.com>
2022-05-03 10:18:14 +0300

    Update sorting_names.py

--- sorting_names.py
+++ sorting_names.py
@@ -1,7 +1,7 @@
 names = input().split(', ')
 
 
-# define a function, returning the sorted names by length (longest to the shortest order, and alphabetically):
+# define a function, returning the sorted names by length (longest to the shortest order, than alphabetically):
 def length_sort(text):
     return -len(text), text
 
