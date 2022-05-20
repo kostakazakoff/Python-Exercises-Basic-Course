2472dab6744744096a47927fbbd10ca610ce3824
Kosta Kazakov <kostakazakoff@gmail.com>
2022-05-21 01:13:53 +0300

    Update coins_2.py

--- coins_2.py
+++ coins_2.py
@@ -1,12 +1,12 @@
-def change(x):
-    x = float(input())
-    x = int(100 * sum)
-    result = 0
+def change(money):
+    money = int(100 * money)
+    coins = 0
     coin_value = (200, 100, 50, 20, 10, 5, 2, 1)
-    for coins in coin_value:
-        result += x // coins
-        x = x % coins
+    for coin in coin_value:
+        coins += money // coin
+        money = money % coin
+        return coins
 
-money = float(input())
-result = change(money)
+m = float(input())
+result = change(m)
 print(result)
